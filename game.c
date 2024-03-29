e0760114f90db42b30148f928d7972e051386816
subodh <subodhlihitkar@gmail.com>
2024-03-29 22:19:03 +0530

    Added basic enemy with collision and gravity

--- game.c
+++ game.c
@@ -1,7 +1,7 @@
 #include <raylib.h>
 
 #define SCREEN_HEIGHT 600
-#define SCREEN_WIDTH 800
+#define SCREEN_WIDTH 1000
 
 #define LEVEL_HEIGHT 12
 #define LEVEL_WIDTH 20
@@ -9,15 +9,20 @@
 #define BLOCK_SIZE 50
 
 #define PLAYER_VELOCITY 500
-#define JUMP_VELOCITY 1000
-#define GRAVITY 50
+#define JUMP_VELOCITY 700
+#define GRAVITY 2000
 
-#define MAX_BULLETS 100 //Maximum number of bullets at any time
-#define BULLET_SPEED 1000
+#define MAX_BULLETS 50 //Maximum number of bullets at any time
+#define BULLET_SPEED 700
 #define BULLET_SIZE 15
 #define MAX_BULLET_FRAMES 60*30 //No. of frames a bullet will last on screen
 
 float delta; //Delta is the time since last frame was drawn
+
+struct Box{
+  int x,y;
+  int width,height;
+};
 
 enum Direction
 {
@@ -45,7 +50,7 @@ enum BlockType
   Ground
 };
 
-struct Block
+struct BlockBehavior
 {
   bool visible;
   bool bullet_collidable;
@@ -57,7 +62,7 @@ struct Block
 };
 
 //Behavior of diffrent types of blocks
-struct Block block[] = {
+struct BlockBehavior block_behavior[] = {
     // Air
     {
       false,
@@ -103,7 +108,7 @@ struct Bullet
   bool exits;
   int x,y;
   int vx,vy;
-  float frames_since_spawned;
+  int frames_since_spawned;
 };
 
 struct Level
@@ -111,6 +116,37 @@ struct Level
   char name[10];
   int block_types[LEVEL_HEIGHT][LEVEL_WIDTH];
 };
+
+enum EnemyType{
+  Follower //Walks towards player
+};
+
+struct EnemyTypeProperties
+{
+  struct Color color;
+  int width,height;
+}; 
+
+struct EnemyTypeProperties enemy_properties[] = {
+  {
+    { 230, 41, 55, 255 }, //RED
+    BLOCK_SIZE,BLOCK_SIZE
+  }
+};
+
+struct Enemy
+{
+  int hp;
+  enum EnemyType type;
+
+  int x, y;
+  int vx, vy;
+  int ay;
+
+  bool is_jumping;
+  enum Direction direction;
+};
+
 
 void display_bullets(struct Bullet *bullet_list);
 void update_bullets(struct Bullet *bullet_list);
@@ -121,6 +157,13 @@ void display_level(struct Level level);
 void player_level_collision(struct Player *player, struct Level level);
 
 void player_controller(struct Player *player, struct Bullet *bullet_list);
+
+void enemy_draw_and_update(struct Enemy *enemy_list,struct Level *level,struct Player *player,struct Bullet *bullet_list);
+
+void draw_enemy(struct Enemy enemy);
+void enemy_update(struct Enemy *enemy);
+void enemy_behavior(struct Enemy *enemy,struct Level *level,struct Player *player,struct Bullet *bullet_list);
+void enemy_level_collision(struct Enemy *enemy,struct Level *level);
 
 int main()
 {
@@ -135,6 +178,7 @@ int main()
 
   struct Level level1 = {
       "level1",
+
       {{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
        {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0},
@@ -168,6 +212,17 @@ int main()
     bullet_list[i].vy = 0;
   }
 
+  struct Enemy enemy1 = {
+    100,
+    Follower,
+    10*BLOCK_SIZE,4*BLOCK_SIZE,
+    0,0,
+    GRAVITY,
+    false,
+    LEFT
+  };
+
+  //Camera to follow player
   cam.target = (Vector2){player.x, 0};
   cam.offset = (Vector2){screenWidth / 2, 0};
   cam.rotation = 0.0f;
@@ -182,10 +237,13 @@ int main()
     ClearBackground(RAYWHITE);
     DrawFPS(20, 20);
 
+    bullet_level_collision(bullet_list,&current_level);
+
     BeginMode2D(cam);
 
     //Draw Player
     DrawRectangle(player.x, player.y, 50, 50, PINK);
+    draw_enemy(enemy1);
 
     //Draw other things
     display_bullets(bullet_list);
@@ -197,9 +255,11 @@ int main()
 
     //Collisions
     player_level_collision(&player, current_level);
-    bullet_level_collision(bullet_list,&current_level);
 
-    player.vy += player.ay;
+    enemy_level_collision(&enemy1,&current_level);
+    enemy_update(&enemy1);
+
+    player.vy += delta * player.ay;
     player.x += player.vx * delta;
     player.y += player.vy * delta;
 
@@ -210,6 +270,113 @@ int main()
   }
   CloseWindow();
   return 0;
+}
+
+void draw_enemy(struct Enemy enemy)
+{
+  struct EnemyTypeProperties type;
+  switch (enemy.type)
+  {
+  case Follower:
+    type = enemy_properties[Follower];
+    DrawRectangle(enemy.x,enemy.y,type.width,type.height,type.color);
+    break;
+  
+  default:
+    break;
+  }
+}
+
+void enemy_level_collision(struct Enemy *enemy,struct Level *level)
+{
+    enum BlockType type_X, type_Y1, type_Y2;
+
+  int collision_X, collision_Y;
+  int level_X,level_Y;
+
+  int midX,midY;
+  midX = enemy->x + BLOCK_SIZE/2;
+  midY = enemy->y + BLOCK_SIZE/2;
+
+  if (enemy->vx > 0)
+  {
+    collision_X = (enemy->x + BLOCK_SIZE);
+  }
+  else if (enemy->vx < 0)
+  {
+    collision_X = (enemy->x);
+  }
+
+  level_X = collision_X/BLOCK_SIZE;
+
+  if(enemy->vy >= 0)
+  {
+    collision_Y = enemy->y + BLOCK_SIZE;
+  }
+  else if(enemy->vy < 0)
+  {
+    collision_Y = enemy->y;
+  }
+
+  level_Y = collision_Y/BLOCK_SIZE;
+
+  if (level_X < 0 || level_X > LEVEL_WIDTH)
+  {
+    enemy->vx = 0;
+    return;
+  }
+
+  if (level_Y < 1 || level_Y > LEVEL_HEIGHT - 1)
+  {
+    enemy->vy = 0;
+    return;
+  }
+
+  type_X = level->block_types[(enemy->y + BLOCK_SIZE/2) / BLOCK_SIZE][collision_X / BLOCK_SIZE];
+
+  type_Y1 = level->block_types[collision_Y / BLOCK_SIZE][(enemy->x + 1) / BLOCK_SIZE];
+  type_Y2 = level->block_types[collision_Y / BLOCK_SIZE][(enemy->x + BLOCK_SIZE - 1) / BLOCK_SIZE];
+
+  if (enemy->vx > 0 && block_behavior[type_X].collidable_sides[LEFT_SIDE])
+  {
+    enemy->x = level_X*BLOCK_SIZE - BLOCK_SIZE;
+    enemy->vx = 0;
+  }
+
+  if (enemy->vx < 0 && block_behavior[type_X].collidable_sides[RIGHT_SIDE])
+  {
+    enemy->x = level_X*BLOCK_SIZE + BLOCK_SIZE - 1;
+    enemy->vx = 0;
+  }
+
+  if (enemy->vy > 0  &&
+    (block_behavior[type_Y1].collidable_sides[TOP_SIDE] || 
+    block_behavior[type_Y2].collidable_sides[TOP_SIDE]))
+  {
+    enemy->y = (level_Y * BLOCK_SIZE) - BLOCK_SIZE;
+
+    enemy->ay = 0;
+    enemy->vy = 0;
+    enemy->is_jumping = false;
+  }else{
+    enemy->ay = GRAVITY;
+  }
+
+  if (enemy->vy < 0  &&
+    (block_behavior[type_Y1].collidable_sides[BOTTOM_SIDE] || 
+    block_behavior[type_Y2].collidable_sides[BOTTOM_SIDE]))
+  {
+    enemy->y = (level_Y * BLOCK_SIZE) + BLOCK_SIZE - 1;
+    enemy->vy = 0;
+  }
+}
+
+void enemy_update(struct Enemy *enemy)
+{
+  enemy->vy += delta*enemy->ay;
+
+  enemy->x += delta*enemy->vx;
+  enemy->y += delta*enemy->vy;
 }
 
 void bullet_level_collision(struct Bullet *bullet_list,struct Level *level)
@@ -225,7 +392,7 @@ void bullet_level_collision(struct Bullet *bullet_list,struct Level *level)
     {
       type = level->block_types[bullet->y / BLOCK_SIZE][ bullet->x / BLOCK_SIZE];
 
-      if(block[type].bullet_collidable)
+      if(block_behavior[type].bullet_collidable)
       {
         bullet->exits = false;
         bullet->vx = 0;
@@ -306,9 +473,9 @@ void display_level(struct Level level)
     {
       type = level.block_types[y][x];
 
-      if (block[type].visible)
+      if (block_behavior[type].visible)
       {
-        DrawRectangle(x * BLOCK_SIZE, y * BLOCK_SIZE, block[type].display_width, block[type].display_height, block[type].color);
+        DrawRectangle(x * BLOCK_SIZE, y * BLOCK_SIZE, block_behavior[type].display_width, block_behavior[type].display_height, block_behavior[type].color);
       }
     }
   }
@@ -364,21 +531,21 @@ void player_level_collision(struct Player *player, struct Level level)
   type_Y1 = level.block_types[collision_Y / BLOCK_SIZE][(player->x + 1) / BLOCK_SIZE];
   type_Y2 = level.block_types[collision_Y / BLOCK_SIZE][(player->x + BLOCK_SIZE - 1) / BLOCK_SIZE];
 
-  if (player->vx > 0 && block[type_X].collidable_sides[LEFT_SIDE])
+  if (player->vx > 0 && block_behavior[type_X].collidable_sides[LEFT_SIDE])
   {
     player->x = level_X*BLOCK_SIZE - BLOCK_SIZE;
     player->vx = 0;
   }
 
-  if (player->vx < 0 && block[type_X].collidable_sides[RIGHT_SIDE])
+  if (player->vx < 0 && block_behavior[type_X].collidable_sides[RIGHT_SIDE])
   {
     player->x = level_X*BLOCK_SIZE + BLOCK_SIZE - 1;
     player->vx = 0;
   }
 
   if (player->vy > 0  &&
-    (block[type_Y1].collidable_sides[TOP_SIDE] || 
-    block[type_Y2].collidable_sides[TOP_SIDE]))
+    (block_behavior[type_Y1].collidable_sides[TOP_SIDE] || 
+    block_behavior[type_Y2].collidable_sides[TOP_SIDE]))
   {
     player->y = (level_Y * BLOCK_SIZE) - BLOCK_SIZE;
 
@@ -390,8 +557,8 @@ void player_level_collision(struct Player *player, struct Level level)
   }
 
   if (player->vy < 0  &&
-    (block[type_Y1].collidable_sides[BOTTOM_SIDE] || 
-    block[type_Y2].collidable_sides[BOTTOM_SIDE]))
+    (block_behavior[type_Y1].collidable_sides[BOTTOM_SIDE] || 
+    block_behavior[type_Y2].collidable_sides[BOTTOM_SIDE]))
   {
     player->y = (level_Y * BLOCK_SIZE) + BLOCK_SIZE - 1;
     //player->y = (level_Y * BLOCK_SIZE) - 1; for jumpable platforms
